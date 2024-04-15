1e0b48703f4992f2ec4447bad54b96b210c8a9a2
symbioids <parasshresthanever@gmail.com>
2024-04-15 19:42:43 +0545

    reducer

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,32 @@
+import { createSlice , nanoid} from "@reduxjs/toolkit";
+
+const initialState = {
+    todo: [{id: 1, text: "Hello world"}]
+}
+
+
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action)=>{
+            const todo = {
+                id:nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state,action)=>{
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+        },
+
+    }
+
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
